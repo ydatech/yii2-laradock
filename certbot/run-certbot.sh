38b668944c3751a87caf2db6570ec4c00f185836
Yuda Sukmana <yudasukmana1992@gmail.com>
2018-01-22 12:28:24 +0700

    update copy life certificate

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -2,5 +2,5 @@
 
 letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
 
-cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/cert1.pem
-cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/privkey1.pem
+cp /etc/letsencrypt/live/"$CN"/cert.pem /var/certs/cert1.pem
+cp /etc/letsencrypt/live/"$CN"/privkey.pem /var/certs/privkey1.pem
