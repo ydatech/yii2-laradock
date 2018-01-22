3ae90f4fe8da939c3d62f2a42ddefe638f4a5341
Yuda Sukmana <yudasukmana1992@gmail.com>
2018-01-22 12:40:31 +0700

    update

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -2,5 +2,5 @@
 
 letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
 
-cp /etc/letsencrypt/live/"$CN"/cert.pem /var/certs/cert1.pem
-cp /etc/letsencrypt/live/"$CN"/privkey.pem /var/certs/privkey1.pem
+cp /etc/letsencrypt/live/"$CN"/cert.pem /var/certs/cert.pem
+cp /etc/letsencrypt/live/"$CN"/privkey.pem /var/certs/privkey.pem
